783aa2937bc00e1d24bdbdf6899867c4c0700d33
Blake Boswell <blake.w.boswell@gmail.com>
2017-07-28 22:46:44 -0500

    Added protected requests and auth guard

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { tokenNotExpired } from 'angular2-jwt';
 
 @Injectable()
 export class AuthService {
@@ -24,12 +25,31 @@ export class AuthService {
     .map(res => res.json());
   }
 
+  getProfile() {
+    let headers = new Headers();
+    this.loadToken();
+    headers.append('Authorization', this.authToken);
+    headers.append('Content-Type','application/json');
+    return this.http.get('http://localhost:8080/users/profile', {headers: headers})
+    .map(res => res.json());
+  }
+
   storeUserData(token, user) {
     localStorage.setItem("id_token", token);
     //localStorage can only store strings, cannot store objects
     localStorage.setItem("user", JSON.stringify(user));
     this.authToken = token;
     this.user = user;
+  }
+
+// fetch token from localStorage so we can getProfile
+  loadToken() {
+    const token = localStorage.getItem("id_token");
+    this.authToken = token;
+  }
+
+  loggedIn() {
+    return tokenNotExpired("id_token");
   }
 
   logout() {
