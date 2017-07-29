63c47397fff692e99588e3f98bae0c3022873bd7
Blake Boswell <blake.w.boswell@gmail.com>
2017-07-28 21:47:11 -0500

    Logging in and out, and storing and deleting JWT in localstorage

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -15,4 +15,26 @@ export class AuthService {
     return this.http.post('http://localhost:8080/users/register', user, {headers: headers})
     .map(res => res.json());
   }
+
+// On success returns token and user info
+  authenticateUser(user) {
+    let headers = new Headers();
+    headers.append('Content-Type','application/json');
+    return this.http.post('http://localhost:8080/users/authenticate', user, {headers: headers})
+    .map(res => res.json());
+  }
+
+  storeUserData(token, user) {
+    localStorage.setItem("id_token", token);
+    //localStorage can only store strings, cannot store objects
+    localStorage.setItem("user", JSON.stringify(user));
+    this.authToken = token;
+    this.user = user;
+  }
+
+  logout() {
+    this.authToken = null;
+    this.user = null;
+    localStorage.clear();
+  }
 }
